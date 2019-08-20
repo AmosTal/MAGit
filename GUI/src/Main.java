91ffd5d656d71ec91f5b313ab21d272d98bc3be7
amost <amostal10@gmail.com>
2019-08-20 16:47:19 +0300

    העברה למחשב חדש

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Resources/a.fxml"));
         primaryStage.setTitle("MAGit");
-        primaryStage.setScene(new Scene(root, 1200,800));
+        primaryStage.setScene(new Scene(root, 600,600));
         primaryStage.show();
         Controller controller = new Controller();
 
