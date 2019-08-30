fbd1fee7f6f422715da7a7bc91f40a1344466b56
Yoni491 <yoniendyoni@gmail.com>
2019-08-30 17:02:55 +0300

    some lines in main changed

--- GUI/src/MainPackage/Main.java
+++ GUI/src/MainPackage/Main.java
@@ -4,15 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL url = getClass().getResource("main.fxml");
+        fxmlLoader.setLocation(url);
+        BorderPane root = fxmlLoader.load(url.openStream());
 
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("MAGit");
         Scene scene = new Scene(root, 1200,800);
         scene.getStylesheets().add("Resources/caspian.css");
