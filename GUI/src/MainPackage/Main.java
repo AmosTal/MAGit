8f53a5ee4045ec290c6158814944b7268360ed57
amost <amostal10@gmail.com>
2019-09-02 09:31:33 +0300

    graphicRep without logic works

--- GUI/src/MainPackage/Main.java
+++ GUI/src/MainPackage/Main.java
@@ -1,9 +1,13 @@
 package MainPackage;
 
+import com.fxgraph.graph.Graph;
+import com.fxgraph.graph.PannableCanvas;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -19,13 +23,27 @@ public class Main extends Application {
         URL url = getClass().getResource("main.fxml");
         fxmlLoader.setLocation(url);
         BorderPane root = fxmlLoader.load(url.openStream());
+        Graph tree = new Graph();
+        GraphicTree.SampleMain.createCommits(tree);
 
 //        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("MAGit");
         Scene scene = new Scene(root, 1200,800);
+        ScrollPane scrollPane = (ScrollPane) scene.lookup("#scrollPane");
+        PannableCanvas canvas = tree.getCanvas();
+        //canvas.setPrefWidth(100);
+        //canvas.setPrefHeight(100);
+        scrollPane.setContent(canvas);
+
         scene.getStylesheets().add("Resources/caspian.css");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+        Platform.runLater(() -> {
+            tree.getUseViewportGestures().set(false);
+            tree.getUseNodeGestures().set(false);
+        });
 
 
     }
